3dfa46c73d120bc975c7765bdcac3dbb2b294964
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-03-26 00:19:58 +0600

    scroll functionality added

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+
+// Scroll Functionality
+document.getElementById('btn-buy-ticket').addEventListener('click', function () {
+  document.getElementById('section-ticket-buy').scrollIntoView({
+    behavior: 'smooth'
+  })
+})
